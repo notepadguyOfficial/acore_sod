1bd3988aa76090a913726295c9462155cfa67d46
notepadguyOfficial <rerollhashtagnumber1@gmail.com>
2025-06-23 15:37:27 +0800

    Update SOD.lua

--- SOD.lua
+++ SOD.lua
@@ -1,4 +1,5 @@
 local PLAYER_EVENT_ON_LOGIN = 3
+local PLAYER_EVENT_ON_LOGOUT = 4
 
     -- 80865 +50%
     -- 80866 +100%
@@ -8,26 +9,14 @@ local PLAYER_EVENT_ON_LOGIN = 3
     -- 80869 +250%
     -- 80870 +300%
 
--- Helper to log messages with player name
-local function Log(player, message)
-	print(string.format("[Discoverer's Delight] [%s] %s", player:GetName(), message))
-end
-
--- Hardcode this shit
-local function RemoveAllAppliedBuffs(player)
-	player:RemoveAura(80865)
-	player:RemoveAura(80866)
-	player:RemoveAura(80867)
-	player:RemoveAura(80868)
-	player:RemoveAura(80869)
-	player:RemoveAura(80870)
-end
-
 -- Player login event
 local function OnLogin(event, player)
-	Log(player, "Player logged in at level " .. player:GetLevel())
-	RemoveAllAppliedBuffs(player)
 	player:CastSpell(player, 80870, true)
 end
 
+local function OnLogout(event, player)
+	player:RemoveAura(80870)
+end
+
 RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, OnLogin)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGOUT, OnLogout)
